02031151e48d64b9dc0f74be951af02a9fa87866
Andrey Devyatkin <ady@praqma.net>
2015-12-04 23:33:09 +0100

    somewhat works using jakson
    Signed-off-by: Andrey Devyatkin <ady@praqma.net>

--- src/main/java/net/praqma/tracey/protocol/BaseEvent.java
+++ src/main/java/net/praqma/tracey/protocol/BaseEvent.java
@@ -1,14 +1,20 @@
 package net.praqma.tracey.protocol;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 public class BaseEvent {
+    private static final Logger log = Logger.getLogger( BaseEvent.class.getName() );
     private String          eventId;
     private long            timestamp;
-    private Source source;
+    private Source          source;
     private List<Reference> references;
+    private ObjectMapper    mapper = new ObjectMapper();
 
     public BaseEvent() {
         this.eventId = UUID.randomUUID().toString();
@@ -48,10 +54,18 @@ public class BaseEvent {
     }
 
     public Boolean hasReferences() {
-        return references.isEmpty() ? false : true;
+        return !references.isEmpty();
     }
 
     public List<Reference> getReferences() {
         return references;
+    }
+
+    public String toJson() throws JsonProcessingException {
+        return mapper.writeValueAsString(this);
+    }
+
+    public String toPrettyJson() throws JsonProcessingException {
+        return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(this);
     }
 }
