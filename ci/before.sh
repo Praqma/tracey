0f441aa232a639ad00d8c323edcfd38c804d3ad0
Andrey Devyatkin <andrey.a.devyatkin@gmail.com>
2016-06-07 14:32:09 +0200

    Added pretested
    Signed-off-by: Andrey Devyatkin <andrey.a.devyatkin@gmail.com>

---
+++ ci/before.sh
@@ -0,0 +1,6 @@
+echo "Building $TRAVIS_BRANCH"
+echo "Commit: $TRAVIS_COMMIT"
+git fetch origin +refs/heads/*:refs/remotes/origin/* --prune
+git branch -a
+git checkout origin/$TARGET_BRANCH
+git merge --ff-only $TRAVIS_COMMIT
